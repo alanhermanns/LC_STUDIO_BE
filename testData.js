d57afc1cc94c3018f112f955dc1778855ca45271
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2020-08-29 14:14:11 -0700

    app d

--- testData.js
+++ testData.js
@@ -1,10 +1,124 @@
-const User = require('./lib/models/User');
-require('./lib/utils/connect')();
+//for cloud function
+// exports = function(changeEvent) {
+    
+//     const ObjectId = BSON.ObjectId;
+//     const collection = context.services.get("Cluster0").db("LCSTUDIODB").collection("users");
+//     collection.insertMany([{
+//     email : 'guderian@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//         ObjectId()
+//     ]
+// },
+// {
+//     email : 'johanson@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//      ObjectId()
+//     ]
 
-const user  = User.create({
-email : 'dog@lclark.edu',
-vocalStudent: false,
-theoryStudent: false,
-percussionStudent: false
-})
-module.exports = user;
+// },{
+//     email : 'mcberry@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//         ObjectId()
+//     ]
+//     },
+//     {
+//     email : 'susansmith@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//         ObjectId()
+//     ]
+//     },
+//     {
+//     email : 'paschal@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//         ObjectId()
+//     ]
+//     },
+//     {
+//     email : 'st@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//       ObjectId()
+//     ]
+//     }
+// ])
+// .catch((err) => console.log(err));
+
+// return;
+
+
+  
+// };
+// db.users.insertMany([{
+//     email : 'guderian@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//         ObjectId()
+//     ]
+// },
+// {
+//     email : 'johanson@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//         ObjectId()
+//     ]
+
+// },{
+//     email : 'mcberry@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//         ObjectId()
+//     ]
+//     },
+//     {
+//     email : 'susansmith@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//         ObjectId()
+//     ]
+//     },
+//     {
+//     email : 'paschal@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//         ObjectId()
+//     ]
+//     },
+//     {
+//     email : 'st@lclark.edu',
+//     vocalStudent: false,
+//     theoryStudent: false,
+//     percussionStudent: false,
+//     myTimeIds : [
+//         ObjectId()
+//     ]
+//     }
+// ]
+// )
+
